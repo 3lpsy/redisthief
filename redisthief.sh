b6f7012d8826ef99e4ffb2f20272a31c05aa9015
3lpsy <elpsy@protonmail.com>
2019-11-08 12:51:21 -0600

    fix

--- redisthief.sh
+++ redisthief.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+IS_DEBUG="0";
+IS_QUIET="0";
+
 REDIS_BIN="redis-cli";
 REDIS_TARGETS="";
 OUTPUT_DIRECTORY="$PWD";
@@ -8,6 +11,17 @@ NO_DOUBLE_DOWNLOAD="0";
 OUTPUT_TO_STOUT_ONLY="0";
 OUTPUT_TO_STOUT_ALSO="0";
 
+function out() {
+    if [ "$IS_QUIET" != "1" ] ;then 
+       echo "$@";
+    fi 
+}
+
+function debugout () {
+    if [ "$IS_DEBUG" == "1" ]; then 
+        echo "$@"
+    fi
+}
 function show_usage() {
     echo "usage: redisthief.sh [arguments]";
     echo "";
@@ -23,6 +37,8 @@ function show_usage() {
     echo "    -s     output all the key/value data to stdout also";
     echo "    -S     output all the key/value data to stdout only";
     echo "    -N     disables double downloading of a key's value. It set, data will be saved under DATA_DIR/KEY_NAME/value.txt instead of DATA_DIR/KEY_NAME/YYYY-MM-DD_hh-mm-ss.value.txt";
+    echo "    -D     enable debug messsages";
+    echo "    -q     enable quiet mode";
     echo "    -h     show this message";
     echo "";
     echo "examples:"
@@ -70,49 +86,49 @@ function perform_theivery() {
         if [ ${#keys} -gt 2 ]; then 
             for k in $(echo -n $keys); do
                 value=""
-                
+
                 if [ "$OUTPUT_TO_STOUT_ONLY" == "1" ]; then 
-                    echo "[+] found key for ${host}:${port}: $k" >&2;
+                    out "[+] found key for ${host}:${port}: $k" >&2;
                 elif [ -d "${OUTPUT_DIRECTORY}/${k}" ]; then 
-                    echo "[+] found old key for ${host}:${port}: $k" >&2;
+                    debugout "[+] found old key for ${host}:${port}: $k" >&2;
                 else
                     echo "[+] found new key for ${host}:${port}: $k" >&2;
                 fi
                  
-                echo "[+] downloading: $k" >&2;
+                debugout "[+] downloading: $connect GET ${k}" >&2;
 
                 value="$($connect GET ${k})";
 
                 if [ "$OUTPUT_TO_STOUT_ONLY" != "1" ]; then 
                     if [ ! -d "${OUTPUT_DIRECTORY}/${k}" ]; then 
-                        echo "[+] creating: ${OUTPUT_DIRECTORY}/${k}" >&2;
+                        debugout "[+] creating: ${OUTPUT_DIRECTORY}/${k}" >&2;
                         mkdir -p "${OUTPUT_DIRECTORY}/${k}";
                     fi
                     if [ "$NO_DOUBLE_DOWNLOAD" != "1" ]; then 
-                        echo "[+] saving $k to ${OUTPUT_DIRECTORY}/${k}/${date}.${host}.${port}.value.txt" >&2;
+                        debugout "[+] saving $k to ${OUTPUT_DIRECTORY}/${k}/${date}.${host}.${port}.value.txt" >&2;
                         echo "$value" > "${OUTPUT_DIRECTORY}/${k}/${date}.${host}.${port}.value.txt"
                     else
                         if [ ! -f "${OUTPUT_DIRECTORY}/${k}/${host}.${port}.value.txt" ]; then 
-                            echo "[+] saving $k to ${OUTPUT_DIRECTORY}/${k}/${host}.${port}.value.txt" >&2;
+                            debugout "[+] saving $k to ${OUTPUT_DIRECTORY}/${k}/${host}.${port}.value.txt" >&2;
                             echo "$value" > "${OUTPUT_DIRECTORY}/${k}/${host}.${port}.value.txt"
                         else
-                            echo "[+] skipping duplicate $k" >&2;
+                            debugout "[+] skipping duplicate $k" >&2;
                         fi
                     fi
                     if [ "$OUTPUT_TO_STOUT_ALSO" == "1" ]; then 
-                        echo "${host}:${port}:KEY=${k}:VALUE=${value}";
+                        echo "HOST=${host}:PORT=${port}:KEY=${k}:VALUE=${value}";
                     fi
                 else
-                    echo "${host}:${port}:KEY=${k}:VALUE=${value}";
+                    echo "HOST=${host}:PORT=${port}:KEY=${k}:VALUE=${value}";
                 fi
             done
         else
-            echo "[+] no keys found for: ${host}:${port} >&2";
+            out "[+] no keys found for: ${host}:${port} >&2";
         fi
     done
 }
 
-while getopts 't:o:r:d:NhSs' OPTION; do
+while getopts 't:o:r:d:NhSsDq' OPTION; do
   case "$OPTION" in
     r)
         REDIS_BIN="$OPTARG"
@@ -136,6 +152,12 @@ while getopts 't:o:r:d:NhSs' OPTION; do
         ;;
     S)
         OUTPUT_TO_STOUT_ONLY="1"
+        ;;
+    S)
+        IS_DEBUG="1"
+        ;;
+    q)
+        IS_QUIET="1"
         ;;
     h)
       show_usage
